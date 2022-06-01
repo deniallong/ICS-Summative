31ada9d8dce4d87f02359d84fdaf2d61488fad28
Daniel <daniel.long@student.tdsb.on.ca>
2022-06-01 12:33:35 -0400

    organized utils, added ImageDisplayer class, and added Main class

---
+++ Main.java
@@ -0,0 +1,13 @@
+import utils.Utils;
+import utils.ImageDisplayer;
+
+public class Main {
+
+    static Console c;
+    static ImageDisplayer displayer;
+    
+    public static void main(String[] args) 
+    {
+        // Main code
+    }
+}
