be3d2c4464eda5ba23d0f234eeb7a44144375b1d
deniallong <d>
2022-06-01 23:48:05 -0400

    revert test commit

--- Main.java
+++ Main.java
@@ -14,7 +14,6 @@ public class Main {
     public static void main(String[] args) throws IOException
     {
         // Main code
-        
     }
 
     public static void ChessDriver() throws IOException 
