48c825291f71f5034e292fd9395220fde9a54f3c
deniallong <d>
2022-06-01 23:38:48 -0400

    Added Bishop class

---
+++ games/chess/Pieces/Bishop.java
@@ -0,0 +1,26 @@
+package games.chess.Pieces;
+
+import games.chess.Structures.*;
+import utils.*;
+
+public class Bishop extends ChessPiece {
+
+    public Bishop(int player)
+    {
+        super(player);
+    }
+
+    public Pair[] getMoves(int[][] openSpaces, int row, int col)
+    {
+        // Declaring variables
+        Pair[] moves;
+
+        // Initializing variables
+        moves = new Pair[0];
+
+        // Processing
+        // Checking which moves are valid
+
+        return moves;
+    }
+}
