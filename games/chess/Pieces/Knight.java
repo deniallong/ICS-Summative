7a2070ad7277763de8c505044d8bad1676e18869
deniallong <d>
2022-06-01 23:49:43 -0400

    added Knight class

---
+++ games/chess/Pieces/Knight.java
@@ -0,0 +1,26 @@
+package games.chess.Pieces;
+
+import games.chess.Structures.*;
+import utils.*;
+
+public class Knight extends ChessPiece {
+
+    public Knight(int player)
+    {
+        super(player);
+    }
+
+    public Pair[] getMoves(int[][] openSpaces, int row, int col)
+    {
+        // Declaring variables
+        Pair[] moves;
+
+        // Initializing variables
+        moves = new Pair[0];
+
+        // Processing
+        // Checking which moves are valid
+
+        return moves;
+    }
+}
