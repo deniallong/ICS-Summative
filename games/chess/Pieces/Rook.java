fa510f48dfd22d531778aa10a94ce9a371886f6b
deniallong <d>
2022-06-01 23:19:18 -0400

    Added rook class

---
+++ games/chess/Pieces/Rook.java
@@ -0,0 +1,27 @@
+package games.chess.Pieces;
+
+import games.chess.Chess;
+
+import utils.Pair;
+
+public class Rook extends ChessPiece {
+
+    public Rook(int player)
+    {
+        super(player);
+    }
+
+    public Pair[] getMoves(int[][] openSpaces, int row, int col)
+    {
+        // Declaring variables
+        Pair[] moves;
+
+        // Initializing variables
+        moves = new Pair[0];
+
+        // Processing
+        // Checking which moves are valid
+        
+        return moves;
+    }
+}
